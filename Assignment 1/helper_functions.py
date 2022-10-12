162d72f31cb180cb2ee597b470c682c3ee67b42e
Akanksha Singh <88332998+akankshasingh25@users.noreply.github.com>
2022-10-12 07:16:12 +0530

    Update helper_functions.py

--- Assignment 1/helper_functions.py
+++ Assignment 1/helper_functions.py
@@ -1,23 +1,22 @@
-"""Helper functions Bug Simulations"""
-
+"""Helper functions for Bug simulations"""
 import math
 
-# Q1
+# Coordinate Geometry
 def get_line(A, B):
-    m= (B[1]- B[0])/ (A[1]- A[0])
-    c= B[0]- m* A[0]
+    m = (B[1] - B[0]) / (A[1] - A[0])
+    c = B[0] - m*A[0]
     return m, c
 
 def distance_between(A, B):
-    return math.sqrt((A[0]- B[0])**2+ (A[1]- B[1])**2)
+    return math.sqrt((A[0] - B[0])**2 + (A[1] - B[1])**2)
 
 def angle_of(A, B):
-    return math.degrees(math.atan2((B[1]- A[1]), (B[0]- A[0]))% 360)+ 90 
+    return math.degrees(math.atan2((B[1]-A[1]), (B[0]-A[0])) % 360) + 90 
     
 def distance_from_line(x, A, B):
-    length_AB= distance_between(A, B)
-    distance_from_AB= abs((B[1]- A[1])*(A[0] - x[0])- (B[0]- A[0])*(A[1]- x[1]))
-    distance_from_AB/= length_AB
+    length_AB = distance_between(A, B)
+    distance_from_AB = abs((B[1]-A[1])*(A[0] - x[0]) - (B[0]-A[0])*(A[1]-x[1]))
+    distance_from_AB /= length_AB
     return distance_from_AB
     
 def distance_from_polygon(x, pairs):
@@ -29,7 +28,7 @@ def distance_from_polygon(x, pairs):
     return min(distances)
 
 
-# For Bug Algorithms
+# For Bug algorithms
 def on_line(x, A, B, tolerance=0.02):   
     dist = distance_from_line(x, A, B)
     if dist > tolerance:
